337e961b6d72bbdca2bef49e8836227892683e66
Patrick Al Khouri <patrick.darya@gmail.com>
2021-02-15 12:40:28 -0300

    Did news seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,19 @@
 News.destroy_all
 
 
+puts 'creating news'
 
 news_1 = News.create(user: User.first, title_es: "Fluminense en Libertadores", title_pt: "Fluminense na libertadores",
-description_es: "El Flumininense llego a Copa Libertadores pela primeira vez desde 2013",
-description_pt: "O Fluminense se classificou para a Copa Libertadores pela primeira vez desde 2013")
+content_es: "El Flumininense llego a Copa Libertadores pela primeira vez desde 2013",
+content_pt: "O Fluminense se classificou para a Copa Libertadores pela primeira vez desde 2013")
+
+news_2 = News.create(user: User.first, title_es: "Vasco en B", title_pt: "Vasco na B",
+content_es: "El Vasco da Gama se caió a la quarta vez para la b",
+content_pt: "O Vasco da Gama caiu pela 4a vez para a 2a divisão do Brasileirão")
+
+  
+news_3 = News.create(user: User.second, title_es: "Palmeiras Campeon", title_pt: "Palmeiras Campeão",
+content_es: "Palmeiras gano por la 2a vez la Copa Liberadores",
+content_pt: "O Palmeiras é bi campeão da liberadores! Venceu o Santos por 1x0")
+
+puts 'news created'
