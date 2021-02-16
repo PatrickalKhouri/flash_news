3fbf5bcfee343a4c4628487d14e2835984a4f6c7
Patrick Al Khouri <patrick.darya@gmail.com>
2021-02-16 20:00:42 -0300

    Included photos for the news

--- app/models/news.rb
+++ app/models/news.rb
@@ -2,7 +2,7 @@ class News < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :grades,  dependent: :destroy
-  has_many :tags, through: :taggings,  dependent: :destroy
+  has_many :tags, through: :taggings
   has_one_attached :photo
 
   validates :content_es, :content_pt, :title_es, :title_pt, presence: true
