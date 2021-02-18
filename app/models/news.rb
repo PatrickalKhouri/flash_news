28780ade307ea351a84862b72f2c91c9e9fd56da
Patrick Khouri <patrick.darya@gmail.com>
2021-02-18 20:34:13 -0300

    doind the tagns and need to solve the navbar

--- app/models/news.rb
+++ app/models/news.rb
@@ -7,4 +7,14 @@ class News < ApplicationRecord
 
   validates :content_es, :content_pt, :title_es, :title_pt, :photo, presence: true
   #validates :description_es, :description_pt length: { minimum: 30 }
+
+  def all_tags=(names)
+    self.tags = names.split(",").map do |name|
+        Tag.where(name: name.strip).first_or_create!
+    end
+  end
+  
+  def all_tags
+    self.tags.map(&:name).join(", ")
+  end
 end
